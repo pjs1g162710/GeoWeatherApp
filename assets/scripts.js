5d2a829b6d60cc8c4993be1cf523fc9e6b03e790
pjs1g162710 <pipsimpson2022@gmail.com>
2025-02-06 20:19:54 +0000

    checking

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,10 +15,7 @@ const API_Key="0bfa5f811840d225cb99d0642abdd3ee";
 let weatherURlByCity;
 
 
-
-
 function btnClick(){
-    debugger
     if(!cityEle.value)
     {
         alert('Please enter the city')
@@ -30,8 +27,7 @@ function btnClick(){
 }
 function getLatLongByCity(weatherURlByCity)
 {
-    debugger;
-    fetch(weatherURlByCity)
+       fetch(weatherURlByCity)
         .then(respone=>respone.json())
         .then(data=>{getByLatLong(data)})
         .catch((error) => console.error("Fetch error:", error));
@@ -49,22 +45,21 @@ function getByLatLong(data)
 }
 function displayByLatLong(data)
 {
-    debugger;
-    console.log(data);
+       console.log(data);
+
 }
 
 //Fetch Weather Data**:
 
 //lat and long are contained within 'data'
 
-let lat = data[0].lat;
-let lon = data[0].lon;
+
 
 //   - After getting the coordinates, use the latitude and longitude to fetch weather data from the OpenWeatherMap Weather API.
 //  - Example API call:  
 //     `https://api.openweathermap.org/data/2.5/weather?lat=51.5074&lon=-0.1278&appid=YOUR_API_KEY`
 
-function getWeather(data); {
+function getWeather(lat, lon) {
 let weatherDataURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_Key}`;
 
 fetch(weatherDataURL)
@@ -73,36 +68,43 @@ fetch(weatherDataURL)
         .catch((error) => console.error("Fetch error:", error));
 }
 
+function convertFtoC(degF) {
+    let degC = (degF- 32) * (5 / 9) ;
+    return degC;
+  }
+
 function displayDataWeather(data)
 {
-    debugger;
     console.log(data);
+    let loc = data[0].name;
+    let degF = data[0].main.temp;
+    let celsius = convertFtoC(degF)
+
+    
+
 }
 
 //**Parse the JSON Response**:
 
-//- Extract the following information from the JSON response:
+//- ;Extract the following information from the JSON response:
 // `name`: The location name.
 // // `main.temp`: The temperature (you may need to convert it to Celsius/Fahrenheit depending on your preference).
 // `weather[0].description`: A short description of the weather conditions.
 
 
-let loc = data[0].name;
+
 
 // The JSON output file format is here: https://openweathermap.org/current
 
 
-let temp = data[0].main.temp; //this is in farenheit
+ //this is in farenheit
 
 //convert farenheit to celsius (code example adapted from here: https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-convert-celsius-to-fahrenheit/16806)
 
 //The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
 //the opposite is (32°F − 32) × 5/9 = 0°C
 
-function convertFtoC(degC) {
-    let degC = (temp- 32) * (5 / 9) ;
-    return degC;
-  }
+
   
   // Change the inputs below to test your code
   convertCtoF(30);
